3341c9a9cd23c8b6d3f9276dacaafaae819c3675
Nick Allred <nallred91@gmail.com>
2019-10-09 22:07:38 -0400

    support putting the app in its own bundle

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import DeviceFrame from './DeviceFrame'
 
 export default (ReactDOM, React, App, id) => {
-    if(window.parent === window) {
-        var DeviceFrameComponent = DeviceFrame(React)
-        ReactDOM.render(<DeviceFrameComponent />, document.getElementById(id))
-    }
-    else {
+    var renderApp = (App) => {
         var observer = new MutationObserver(() => {
             if(window.location.href !== window.parent.location.href) {
                 window.parent.history.replaceState(null, '', window.location.href)
@@ -13,5 +9,18 @@ export default (ReactDOM, React, App, id) => {
         })
         observer.observe(document.getElementById(id), { attributes: true, childList: true, subtree: true })
         ReactDOM.render(App, document.getElementById(id));
+    }
+    
+    if(window.parent === window) {
+        var DeviceFrameComponent = DeviceFrame(React)
+        ReactDOM.render(<DeviceFrameComponent />, document.getElementById(id))
+    }
+    else {
+        if(typeof App === 'function') {
+            App().then(renderApp)
+        }
+        else {
+            renderApp(App)
+        }
     }
 }
