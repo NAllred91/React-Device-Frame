b2ada5283660b35e530b064d43435d29c9bd2f52
Nick Allred <nallred91@gmail.com>
2019-10-09 21:07:18 -0400

    webpack build

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: './index.js',
+    module: {
+      rules: [
+        {
+            test: /\.(js)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        },
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+      ]
+    },
+    resolve: {
+      extensions: ['.js']
+    },
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+  }
