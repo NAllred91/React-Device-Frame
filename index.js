f2249822b5ae8c2c34e61971e2b7f6fad5b3df80
Nick Allred <nallred91@gmail.com>
2019-10-09 22:11:49 -0400

    undo previous commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import DeviceFrame from './DeviceFrame'
 
 export default (ReactDOM, React, App, id) => {
-    var renderApp = (App) => {
+    if(window.parent === window) {
+        var DeviceFrameComponent = DeviceFrame(React)
+        ReactDOM.render(<DeviceFrameComponent />, document.getElementById(id))
+    }
+    else {
         var observer = new MutationObserver(() => {
             if(window.location.href !== window.parent.location.href) {
                 window.parent.history.replaceState(null, '', window.location.href)
@@ -9,18 +13,5 @@ export default (ReactDOM, React, App, id) => {
         })
         observer.observe(document.getElementById(id), { attributes: true, childList: true, subtree: true })
         ReactDOM.render(App, document.getElementById(id));
-    }
-    
-    if(window.parent === window) {
-        var DeviceFrameComponent = DeviceFrame(React)
-        ReactDOM.render(<DeviceFrameComponent />, document.getElementById(id))
-    }
-    else {
-        if(typeof App === 'function') {
-            App().then(renderApp)
-        }
-        else {
-            renderApp(App)
-        }
     }
 }
